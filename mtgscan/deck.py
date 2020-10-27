5bfb1c47368da6d794af79ad118151d8fe9817b9
fortierq <qpfortier@gmail.com>
2020-10-27 16:10:14 +0100

    Convert text to cards

--- mtgscan/deck.py
+++ mtgscan/deck.py
@@ -19,17 +19,40 @@ class Pile:
             s += f"{self.cards[card]} {card}\n"
         return s
 
+    def __len__(self):
+        return sum(self.cards[c] for c in self.cards)
+
 class Deck:
     def __init__(self):
         self.maindeck = Pile()
         self.sideboard = Pile()
-    
-    def add_cards(self, cards, in_sideboard = False):
-        if in_sideboard:
-            self.sideboard.add_cards(cards)
-        else:
-            self.maindeck.add_cards(cards)
 
     def __str__(self):
-        return str(self.maindeck) + "\n" + str(self.sideboard)
+        if len(self.sideboard) == 0:
+            return str(self.maindeck)
+        else:
+            return str(self.maindeck) + "\n" + str(self.sideboard)
+    
+    def __len__(self):
+        return len(self.maindeck) + len(self.sideboard)
 
+    def add_card(self, card, in_sideboard):
+        if in_sideboard:
+            self.sideboard.add_card(card)
+        else:
+            self.maindeck.add_card(card)
+
+    def add_cards(self, cards, in_sideboard):
+        for card in cards:
+            self.add_card(card, in_sideboard)
+
+    def save(self, file):
+        with open(file, "w") as f:
+            f.write(str(self))
+
+    def load(self, file):
+        with open(file, "r") as f:
+            in_sideboard = False
+            for line in f:
+                if line == "\n": in_sideboard = True
+                self.add_card(line, in_sideboard)
--- mtgscan/text.py
+++ mtgscan/text.py
@@ -1,64 +1,77 @@
-
 import json
 import logging
 from symspellpy import SymSpell, Verbosity
-import deck
+import mtgscan.deck
 
 class TextRecognition:
 
-    def __init__(self, file_all_cards, verbose = False):
+    def __init__(self, file_all_cards):
         f = open(file_all_cards, "r")
         all_cards_json = json.load(f)
         self.all_cards = dict()
-        for c in all_cards_json:
-            card = TextRecognition.preprocess(c['name'])
+        for c in all_cards_json["data"].keys():
+            card = self.preprocess(c)
             i = card.find("//")
             if i != -1: card = card[:i]
-            self.all_cards[card] = c['name']
+            self.all_cards[card] = c
         f.close()
 
         self.sym_spell = SymSpell(max_dictionary_edit_distance=5)
         for c in self.all_cards: 
             self.sym_spell.create_dictionary_entry(c, 1)
 
-        self.verbose = verbose
-
-    @staticmethod
-    def preprocess(card):
+    def preprocess(self, text):
         rm = " @*()?"
-        card = card[:15]
+        text = text[:15]
         for c in rm:
-            card = card.replace(c, "")
-        return card.lower()
+            text = text.replace(c, "")
+        return text.lower()
 
-    def log(self, msg):
-        if self.verbose: 
-            logging.info(msg)
+    def preprocess_texts(self, texts):
+        texts_proc = []
+        for text in texts:
+            if len(text) < 30:
+                texts_proc.append(self.preprocess(text))
+            else: 
+                logging.info(text)
+        return texts_proc
 
+    def ocr_to_deck(self, texts, boxes):
+        L = list(zip(boxes, texts))
+        L.sort()
+
+        boxes, texts = zip(*L)
+        texts_proc = self.preprocess_texts(texts)
+        cards = self.texts_to_cards(texts_proc)
+
+        deck = mtgscan.deck.Deck()
+        sb = max(60, len(cards) - 15)
+        deck.add_cards(cards[:sb], in_sideboard=False)
+        deck.add_cards(cards[sb:], in_sideboard=True)
+        return deck
+
+    def text_to_card(self, text):
+        if len(text) < 3: 
+            return None
+        if text in self.all_cards:
+            return self.all_cards[text]
+        else:
+            sug = self.sym_spell.lookup(text,Verbosity.CLOSEST,max_edit_distance=5)
+            if sug != []:
+                card = self.all_cards[sug[0].term]
+                ratio = sug[0].distance/len(text)
+                if ratio < 0.25 or (card in cards and ratio < 0.4): 
+                    logging.info(f"Found: {text} {ratio} {card}")
+                    return card
+                else: 
+                    logging.info(f"Not found: {text} {ratio} {card}")
+            else: 
+                logging.info(f"Not found: {text}")
+        return None
+        
     def texts_to_cards(self, texts):
         cards = []
         for text in texts:
-            if len(text) < 3: continue
-            if text in self.all_cards:
-                card = self.all_cards[text]
-                cards.append(card)
-            else:
-                sug = self.sym_spell.lookup(text,Verbosity.CLOSEST,max_edit_distance=5)
-                if sug != []:
-                    card = self.all_cards[sug[0].term]
-                    ratio = sug[0].distance/(len(text))
-                    if ratio < 0.25 or card in cards: cards.append(card)
-                    else: self.log(f"Not found: {text} {ratio} {card}")
-                else: self.log(f"Not found: {text}")
+            cards.append(self.text_to_card(text))
         return cards
-
-    def cards_to_deck(self, cards, has_sideboard = True):
-        maindeck, sideboard = deck.Deck(), deck.Deck()
-        if has_sideboard:
-            maindeck.add_cards(cards[:-15])
-            sideboard.add_cards(cards[-15:])
-            return maindeck, sideboard
-        else:
-            maindeck.add_cards(cards)
-            return maindeck
     
