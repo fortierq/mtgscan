80886f0defd206417283bbbc33bdd4b4a6c91ca5
fortierq <qpfortier@gmail.com>
2020-10-28 18:06:58 +0100

    Add OCR class

--- mtgscan/ocr.py
+++ mtgscan/ocr.py
@@ -3,8 +3,32 @@ import requests
 import time
 import logging
 
-class AzureRecognition:
+class OCR:
+    def save_box_texts(self, file):
+        logging.info(f"Save box_texts to {file}")
+        with open(file, "w") as f:
+            for box, text in self.box_texts:
+                f.write(' '.join(map(str, box)))
+                f.write("\n")
+                f.write(text)
+                f.write("\n")
 
+    def load_box_texts(self, file):
+        logging.info(f"Load box_texts to {file}")
+        self.box_texts = []
+        with open(file, "r") as f:
+            while True:
+                box = f.readline().rstrip('\n')
+                if box == "":
+                    return self.box_texts
+                text = f.readline().rstrip('\n')
+                self.box_texts.append([list(map(int, box.split(" "))), text])
+    
+    def save_box_texts_img(self, file):
+        logging.info(f"Save box_texts image to {file}")
+        
+
+class AzureOCR(OCR):
     def __init__(self):
         self.subscription_key = os.environ['AZURE_VISION_KEY']
         self.text_recognition_url = os.environ['AZURE_VISION_ENDPOINT'] + "/vision/v3.1/read/analyze"
@@ -30,23 +54,3 @@ class AzureRecognition:
         for line in analysis["analyzeResult"]["readResults"][0]["lines"]:
             self.box_texts.append([line["boundingBox"], line["text"]])
         return self.box_texts
-
-    def save_box_texts(self, file):
-        logging.info(f"Save box_texts to {file}")
-        with open(file, "w") as f:
-            for box, text in self.box_texts:
-                f.write(' '.join(map(str, box)))
-                f.write("\n")
-                f.write(text)
-                f.write("\n")
-
-    def load_box_texts(self, file):
-        logging.info(f"Load box_texts to {file}")
-        self.box_texts = []
-        with open(file, "r") as f:
-            while True:
-                box = f.readline().rstrip('\n')
-                if box == "":
-                    return self.box_texts
-                text = f.readline().rstrip('\n')
-                self.box_texts.append([list(map(int, box.split(" "))), text])
--- tests/test.py
+++ tests/test.py
@@ -10,8 +10,8 @@ import logging
 
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
 
-ocr_azure = mtgscan.ocr.AzureRecognition()
-text_rec = mtgscan.text.MagicRecognition("mtgscan/AtomicCards.json")
+ocr_azure = mtgscan.ocr.AzureOCR()
+text_rec = mtgscan.text.MagicRecognition("mtgscan/VintageAtomic.json")
 errors = []
 with os.scandir(os.path.join(DIR_FILE, 'samples')) as samples:
     for sample in samples:
@@ -24,11 +24,11 @@ with os.scandir(os.path.join(DIR_FILE, 'samples')) as samples:
             path_box_texts = os.path.join(sample, "box_texts.txt")
             if not os.path.isfile(path_box_texts):
                 ocr_azure.img_to_box_texts(f_url.read())
-                ocr_azure.save_box_texts(box_texts, path_box_texts)
+                ocr_azure.save_box_texts(path_box_texts)
             else: 
                 ocr_azure.load_box_texts(path_box_texts)
         deck_ocr = text_rec.ocr_to_deck(ocr_azure.box_texts)
         deck_ocr.save(os.path.join(sample, "deck_azure.txt"))
         deck = mtgscan.deck.Deck()
         deck.load(os.path.join(sample, "deck.txt"))
-        print(f"Erreur: {deck.diff(deck_ocr)/len(deck)}")
+        print(f"Erreurs: {deck.diff(deck_ocr)}")
