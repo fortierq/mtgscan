6f302abc9a6d5fd4e1a69d8cea89951d5c7a86a2
Quentin Fortier <qpfortier@gmail.com>
2021-03-23 22:06:21 +0100

    Add run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+from mtgscan.text import MagicRecognition
+from mtgscan.ocr.azure import Azure
+
+azure = Azure()
+rec = MagicRecognition(file_all_cards="all_cards.txt", file_keywords="Keywords.json")
+box_texts = azure.image_to_box_texts("https://user-images.githubusercontent.com/49362475/105632710-fa07a180-5e54-11eb-91bb-c4710ef8168f.jpeg")
+deck = rec.box_texts_to_deck(box_texts)
+print(deck)
