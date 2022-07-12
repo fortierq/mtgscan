98036cb64d9cfab1c974f391260da521b43bdf24
Quentin Fortier <qpfortier@gmail.com>
2022-07-12 15:52:47 +0000

    Use env

--- mtgscan/ocr/azure.py
+++ mtgscan/ocr/azure.py
@@ -10,12 +10,10 @@ from .ocr import OCR
 
 
 class Azure(OCR):
-    def __init__(self,
-                 azure_vision_key=os.environ['AZURE_VISION_KEY'],
-                 azure_vision_endpoint=os.environ['AZURE_VISION_ENDPOINT']):
+    def __init__(self):
         try:
-            self.subscription_key = azure_vision_key
-            self.text_recognition_url = azure_vision_endpoint + "/vision/v3.1/read/analyze"
+            self.subscription_key = os.environ['AZURE_VISION_KEY']
+            self.text_recognition_url = os.environ['AZURE_VISION_ENDPOINT'] + "/vision/v3.1/read/analyze"
         except IndexError as e:
             print(str(e))
             print(
