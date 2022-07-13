b037e2c74cbc5825046a6bada2f17537f52684e7
Quentin Fortier <qpfortier@gmail.com>
2022-07-13 15:06:56 +0000

    Encode

--- mtgscan/utils.py
+++ mtgscan/utils.py
@@ -16,4 +16,4 @@ def load_url_or_file(image: str) -> Image.Image:
         response = requests.get(image)
         response.raise_for_status()
         image = BytesIO(response.content)
-    return np.asarray(Image.open(BytesIO(image)))
+    return np.asarray(Image.open(bytes(image, encoding="utf-8")))
