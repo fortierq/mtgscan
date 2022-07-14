4c6a0f5de8a5ab3fca135b5a2f9aff1139150499
Quentin Fortier <qpfortier@gmail.com>
2022-07-14 17:25:40 +0000

    Load file

--- mtgscan/utils.py
+++ mtgscan/utils.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 from urllib.parse import urlparse
 import base64
+from pathlib import Path
 
 import numpy as np
 import requests
@@ -12,12 +13,22 @@ def is_url(text: str) -> bool:
     return len(parsed.scheme) > 1
 
 
-def load_url_or_base64(image: str) -> Image.Image:
-    """Load an image as numpy array from an url or base64 string"""
+def load_url_or_file_or_base64(image: str) -> Image.Image:
+    """Load an image
+
+    Parameters
+    ----------
+    image : str
+        Url or path or base64 encoded image
+
+    Returns
+    -------
+    Numpy array representing the image
+    """
     if is_url(image):
         response = requests.get(image)
         response.raise_for_status()
         image = BytesIO(response.content)
-    else:
+    elif not Path(image).exists():
         image = BytesIO(base64.b64decode(image))
     return np.asarray(Image.open(image))
